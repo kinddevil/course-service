e0d3c778d2249857cbed6dd40514772590d47557
yichen <yichen.wei@hulu.com>
2017-12-09 16:45:38 +0800

    fix unused class

--- oauth/src/main/java/com/oauth/services/repository/RoleRepository.java
+++ oauth/src/main/java/com/oauth/services/repository/RoleRepository.java
@@ -2,7 +2,6 @@ package com.oauth.services.repository;
 
 import com.oauth.services.domain.Role;
 import com.oauth.services.domain.User;
-import com.oauth.services.domain.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
